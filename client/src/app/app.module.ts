666c25d95b57b1b23e0050080e0e49875b384742
Waleed Alhasan <walcom2kx@gmail.com>
2020-12-29 06:23:33 +0300

    beginning angular service

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,18 +5,21 @@ import { HttpClientModule} from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NavBarComponent } from './nav-bar/nav-bar.component';
+
+import { CoreModule } from './core/core.module';
+// import { NavBarComponent } from './core/nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent
+    // NavBarComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule, 
-    HttpClientModule
+    BrowserAnimationsModule,
+    HttpClientModule,
+    CoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ client/src/app/shop/shop.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { IPagination } from '../shared/models/pagination';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ShopService {
+  baseUrl = 'https://localhost:5001/api/';
+
+  constructor(private http: HttpClient) { }
+
+  // tslint:disable-next-line: typedef
+  getProducts(){
+    return this.http.get<IPagination>(this.baseUrl + 'products');
+  }
+}
