98cd6aae824186b3a45425290d699dc11a4e3737
Waleed Alhasan <walcom2kx@gmail.com>
2021-02-05 23:34:15 +0300

    end of error handling section ( 2021/02/05)

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpClientModule} from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS} from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,6 +10,8 @@ import { CoreModule } from './core/core.module';
 import { ShopModule } from './shop/shop.module';
 // import { NavBarComponent } from './core/nav-bar/nav-bar.component';
 import { HomeModule } from './home/home.module';
+import { ErrorInterceptor } from './core/interceptors/error.interceptor';
+import { ToastrModule } from 'ngx-toastr';
 
 @NgModule({
   declarations: [
@@ -17,15 +19,22 @@ import { HomeModule } from './home/home.module';
     // NavBarComponent
   ],
   imports: [
-    BrowserModule,
+  BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     HttpClientModule,
     CoreModule,
     ShopModule,
-    HomeModule
+    HomeModule,
+    ToastrModule.forRoot({
+      positionClass: 'toast-bottom-right',
+      toastClass: 'toast toast-bootstrap-compatibility-fix',
+      preventDuplicates: true
+    })
   ],
-  providers: [],
+  providers: [
+    {provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- client/src/app/core/test-error/test-error.component.ts
+++ client/src/app/core/test-error/test-error.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { environment } from './../../../environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-test-error',
@@ -6,10 +9,55 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./test-error.component.scss']
 })
 export class TestErrorComponent implements OnInit {
+  baseUrl = environment.apiUrl;
+  validationErrors: any;
 
-  constructor() { }
+  constructor(private http: HttpClient, private toast: ToastrService) { }
 
   ngOnInit(): void {
   }
+
+  // tslint:disable-next-line: typedef
+  get404Error() {
+    this.http.get(this.baseUrl + 'products/42').subscribe(response => {
+      console.log(response);
+    }, error => {
+      console.log(error);
+    });
+  }
+
+
+  // tslint:disable-next-line: typedef
+  get500Error() {
+    this.http.get(this.baseUrl + 'buggy/servererror').subscribe(response => {
+      console.log(response);
+    }, error => {
+      console.log(error);
+    });
+  }
+
+  // tslint:disable-next-line: typedef
+  get400Error() {
+    this.http.get(this.baseUrl + 'buggy/badrequest').subscribe(response => {
+      console.log(response);
+    }, error => {
+      console.log(error);
+    });
+  }
+
+    // tslint:disable-next-line: typedef
+    get400ValidationError() {
+      this.http.get(this.baseUrl + 'products/fourty').subscribe(response => {
+        console.log(response);
+      }, error => {
+        console.log(error);
+        this.validationErrors = error.errors;
+      });
+    }
+
+    // tslint:disable-next-line: typedef
+    testToast() {
+      this.toast.success('I am toast', 'success');
+    }
 
 }
