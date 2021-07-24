54fd8c83f1f27f4faba811ddaff5e62a47fd37c3
Waleed Alhasan <walcom2kx@gmail.com>
2021-07-24 22:10:28 +0300

    End of API-Validation (20210724)

--- API/Dtos/RegisterDto.cs
+++ API/Dtos/RegisterDto.cs
@@ -1,9 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace API.Dtos
 {
     public class RegisterDto
     {
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
-        public string DisplayName { get; set; }        
+
+        [Required]        
+        public string DisplayName { get; set; }
+
+        [Required]
+        [RegularExpression(@"(?=^.{6,10}$)(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&amp;*()_+}{&quot;:;'?/&gt;.&lt;,])(?!.*\s).*$", 
+        ErrorMessage = "Password must have 1 uppercase, 1 lowercase, 1 number, 1 non-aplhanumeric and at least 6 characters")]
         public string Password { get; set; }   
     }
 }
